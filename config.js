762a76a476cb7785ceac40bbd9ec7d1aea0195ee
soberbat <beratgenc470@gmail.com>
2024-02-24 20:23:29 +0300

    removed comments v2

--- config.js
+++ config.js
@@ -9,25 +9,12 @@ const corsOptions = {
 
 const sessionOptions = {
   store: redisStore,
-  // name: "sessionCookie",
   secret: "mysecret",
   saveUninitialized: false,
   resave: false,
-  // cookie: {
-  //   path: "/",
-  //   domain: "auth-test.site",
-  //   maxAge: 1000 * 60 * 30,
-  //   sameSite: "none",
-  //   secure: true,
-  //   httpOnly: true,
-  // },
   cookie: {
     path: "/",
-    // domain: "auth-test.site",
     maxAge: 1000 * 60 * 30,
-    // sameSite: "none",
-    // secure: true,
-    // httpOnly: true,
   },
 };
 
